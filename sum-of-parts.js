84436e14474c82c3c3a25fe6788a0bdb008ead1a
Andrii Maryskevych <andrii.maryskecych@gmail.com>
2019-07-14 16:14:11 +0300

    Add working sum function

---
+++ sum-of-parts.js
@@ -0,0 +1,24 @@
+/*
+Let us consider this example (array written in general format):
+
+ls = [0, 1, 3, 6, 10]
+
+Its following parts:
+
+ls = [0, 1, 3, 6, 10]
+ls = [1, 3, 6, 10]
+ls = [3, 6, 10]
+ls = [6, 10]
+ls = [10]
+ls = []
+
+*/
+
+function partsSums(array) {
+    return array.reduceRight((a, c) => {
+        a.unshift(c + a[0]);
+        return a;
+    }, [0])
+};
+
+console.log(partsSums([744125, 935, 407, 454, 430, 90, 144, 6710213, 889, 810, 2579358]));
