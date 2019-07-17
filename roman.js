d6f5c3a2d92a1e1b6793d7b01a3ae7261afd3a5e
Andrii Maryskevych <andrii.maryskecych@gmail.com>
2019-07-17 07:03:27 +0300

    Wrote encoder

--- roman.js
+++ roman.js
@@ -1,25 +1,39 @@
 // No need to validate input, it is always correct
 
-const romanNumbers = {
-    I: 1,
-    V: 5,
-    X: 10,
-    L: 50,
-    C: 100,
-    D: 500,
-    M: 1000,
-    // Unexisted data for mock purpose
-    XX: 5000,
-    XXX: 10000,
-};
+const romanNumbers = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
 
 const arabianNumbers = { thousand: 1000, hundred: 100, ten: 10, one: 1 };
+const arabRomeMap = {
+    thousand: { low: 'M'},
+    hundred: { low: 'C', middle: 'D', top: 'M' },
+    ten: { low: 'X', middle: 'L', top: 'C' },
+    one: { low: 'I', middle: 'V', top: 'X' },
+};
 const orderdArabianNames = [
     'thousand',
     'hundred',
     'ten',
     'one'
-];
+]
+
+const abstractTransformer = (value, { low, middle, top }) => {
+    switch (value) {
+        case 1:
+        case 2:
+        case 3:
+            return `${low}`.repeat(value);
+        case 4:
+            return `${low}${middle}`;
+        case 5:
+            return `${middle}`;
+        case 6:
+        case 7:
+        case 8:
+            return `${middle}`.concat(`${low}`.repeat(value - 5));
+        case 9:
+            return `${low}${top}`;
+    }
+};
 
 class RomanNumerals {
     static fromRoman (string) {
@@ -38,36 +52,23 @@ class RomanNumerals {
             return a;
         }, {});
 
-        orderdArabianNames.reduce
+        return orderdArabianNames.reduce((a, c) => {
+            if (parsed[c]) {
+                return a.concat(abstractTransformer(parsed[c], arabRomeMap[c]));
+            }
+
+            return a;
+        }, '');
     }
 }
 
-RomanNumerals.toRoman(1234);
+console.log(RomanNumerals.toRoman(1234));
 
-function abstractTransformer(value, { low, middle, top }) {
-    switch (value) {
-        case 1:;
-        case 2:;
-        case 3:;
-            return `${low}`.repeat(value);
-        case 4:
-            return `${low}${middle}`;
-        case 5:
-            return `${middle}`;
-        case 6:;
-        case 7:;
-        case 8:
-            return `${middle}`.concat(`${low}`.repeat(value - 5));
-        case 9:
-            return `${low}${top}`;
-    }
-}
-
-for(let i = 1; i < 10; i++) {
-console.log(abstractTransformer(i, {
-    low: 'X',
-    middle: 'L',
-    top: 'C',
-}))
-}
+// for(let i = 1; i < 10; i++) {
+//     console.log(abstractTransformer(i, {
+//         low: 'C',
+//         middle: 'D',
+//         top: 'M',
+//     }))
+// };
 
