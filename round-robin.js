34aa07204b4856978b359ba292a6b24e205414e0
Andrii Maryskevych <andrii.maryskevych@globallogic.com>
2019-07-17 16:31:22 +0300

    Create all possible pairs

--- round-robin.js
+++ round-robin.js
@@ -1,18 +1,26 @@
-const getNext = (taken, notTaken, maxLength) => {
-    if (maxLength === 0) {
-        return [ taken ];
+const getAllPairs = (count) => {
+    const getNext = (taken, notTaken, maxLength) => {
+        if (maxLength === 0) {
+            return [ taken ];
+        }
+
+        let res = [];
+
+        for (let i = 0; i < notTaken.length - maxLength + 1; i++) {
+            const value = notTaken[i];
+            const shorterArray = notTaken.slice(i + 1);
+
+            res = res.concat(getNext(taken.concat(value), shorterArray, maxLength - 1));
+        }
+
+        return res;
     }
 
-    let res = [];
+    const arr = [];
+    for (let i = 0; i < count; i++) arr.push(i + 1);
 
-    for (let i = 0; i < notTaken.length - maxLength + 1; i++) {
-        const value = notTaken[i];
-        const shorterArray = notTaken.slice(i + 1);
-
-        res = res.concat(getNext(taken.concat(value), shorterArray, maxLength - 1));
-    }
-
-    return res;
+    return getNext([], arr, 2);
 }
 
-console.log(getNext([], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2));
+
+console.log(getAllPairs(10));
