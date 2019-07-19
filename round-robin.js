ae3958f2eeba66988d1b8b02a6e19105e551c846
Andrii Maryskevych <andrii.maryskevych@globallogic.com>
2019-07-19 14:15:02 +0300

    Codewars style

--- round-robin.js
+++ round-robin.js
@@ -1,26 +1,30 @@
-const getAllPairs = (count) => {
-    const getNext = (taken, notTaken, maxLength) => {
-        if (maxLength === 0) {
-            return [ taken ];
+function buildMatchesTable(numberOfTeams) {
+    const getAllPairs = (count) => {
+        const getNext = (taken, notTaken, maxLength) => {
+            if (maxLength === 0) {
+                return [ taken ];
+            }
+
+            let res = [];
+
+            for (let i = 0; i < notTaken.length - maxLength + 1; i++) {
+                const value = notTaken[i];
+                const shorterArray = notTaken.slice(i + 1);
+
+                res = res.concat(getNext(taken.concat(value), shorterArray, maxLength - 1));
+            }
+
+            return res;
         }
 
-        let res = [];
+        const arr = [];
+        for (let i = 0; i < count; i++) arr.push(i + 1);
 
-        for (let i = 0; i < notTaken.length - maxLength + 1; i++) {
-            const value = notTaken[i];
-            const shorterArray = notTaken.slice(i + 1);
-
-            res = res.concat(getNext(taken.concat(value), shorterArray, maxLength - 1));
-        }
-
-        return res;
+        return getNext([], arr, 2);
     }
 
-    const arr = [];
-    for (let i = 0; i < count; i++) arr.push(i + 1);
 
-    return getNext([], arr, 2);
+    console.log(getAllPairs(numberOfTeams));
 }
 
-
-console.log(getAllPairs(10));
+buildMatchesTable(10)
