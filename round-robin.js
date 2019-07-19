851d3213225da0bb4ce55f768173bdf615fffd0f
Andrii Maryskevych <andrii.maryskevych@globallogic.com>
2019-07-19 16:26:39 +0300

    Solved round-robin

--- round-robin.js
+++ round-robin.js
@@ -1,30 +1,78 @@
+// No need to store two data arrays (alreadyUsed, currentStep)
+// Just add all selected values to one array
+// With simple computations it would be possible to apply more strict filters to last n selected points
+
 function buildMatchesTable(numberOfTeams) {
-    const getAllPairs = (count) => {
-        const getNext = (taken, notTaken, maxLength) => {
-            if (maxLength === 0) {
-                return [ taken ];
+    const gamesPerRound = numberOfTeams / 2;
+    const roundsCount = numberOfTeams - 1;
+    const totalGames = gamesPerRound * roundsCount;
+
+    const alreadyTaken = ([x1, y1], [x2, y2]) => x1 === x2 && y1 === y2;
+    const intersection = ([x1, y1], [x2, y2]) => x1 === x2 || y1 === y2 || x1 === y2 || y1 === x2;
+
+    const getPossiblePlaces = (alreadyUsed) => {
+        const res = [];
+
+        for (let i = 0; i < numberOfTeams; i++) {
+            for (let j = i + 1; j < numberOfTeams; j++) {
+                const testPoint = [i, j];
+
+                if (alreadyUsed.some(taken => alreadyTaken(taken, testPoint))) {
+                    continue;
+                }
+
+                const totalTakenPointsCount = alreadyUsed.length;
+                const currentRoundGamesCount = totalTakenPointsCount % gamesPerRound;
+                const lastRoundGames = alreadyUsed.slice(alreadyUsed.length - currentRoundGamesCount);
+
+                if (lastRoundGames.some(taken => intersection(taken, testPoint))) {
+                    continue;
+                }
+
+                res.push([i, j]);
             }
-
-            let res = [];
-
-            for (let i = 0; i < notTaken.length - maxLength + 1; i++) {
-                const value = notTaken[i];
-                const shorterArray = notTaken.slice(i + 1);
-
-                res = res.concat(getNext(taken.concat(value), shorterArray, maxLength - 1));
-            }
-
-            return res;
         }
 
-        const arr = [];
-        for (let i = 0; i < count; i++) arr.push(i + 1);
+        return res;
+    };
 
-        return getNext([], arr, 2);
+    const arrangeGames = (placed = []) => {
+        if (placed.length === totalGames) {
+            return placed;
+        }
+
+        const nextPositions = getPossiblePlaces(placed);
+
+        if (nextPositions.length === 0) {
+            return false;
+        }
+
+        for (let i = 0; i < nextPositions.length; i++) {
+            const copy = placed.concat();
+            copy.push(nextPositions[i]);
+
+            const stepValue = arrangeGames(copy);
+
+            if (stepValue === false) {
+                continue;
+            } else {
+                return stepValue;
+            }
+        }
+
+        return false;
+    };
+
+    const games = arrangeGames().map(([x, y]) => [x + 1, y + 1]);
+
+    const res = [];
+
+    for (let i = 0; i < roundsCount; i++) {
+        const round = games.slice(i * gamesPerRound, gamesPerRound * i + gamesPerRound);
+        res.push(round);
     }
 
-
-    console.log(getAllPairs(numberOfTeams));
+    return res;
 }
 
-buildMatchesTable(10)
+console.log(buildMatchesTable(2));
