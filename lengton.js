e41ab9a97a005f03074e17fa260623fbbc74418c
Andrii Maryskevych <andrii.maryskevych@globallogic.com>
2019-07-24 16:19:12 +0300

    Solved

--- lengton.js
+++ lengton.js
@@ -1,25 +1,10 @@
 const extendArray = (arr, direction) => {
     switch (direction) {
-        case 0: arr.shift(new Array(arr[0].length).fill(0)); break;
+        case 0: arr.unshift(new Array(arr[0].length).fill(0)); break;
         case 1: arr.forEach(subarray => { subarray.push(0); }); break;
         case 2: arr.push(new Array(arr[0].length).fill(0)); break;
-        case 3: arr.forEach(subarray => { subarray.shift(0); }); break;
+        case 3: arr.forEach(subarray => { subarray.unshift(0); }); break;
     }
-
-    const newSize = arr.length + 2;
-
-    const result = [];
-    for (let i = 0; i < newSize; i++) {
-        result.push(new Array(newSize).fill(0));
-    }
-
-    for (let i = 1; i < newSize - 1; i++) {
-        for (let j = 1; j < newSize - 1; j++) {
-            result[i][j] = arr[i-1][j-1];
-        }
-    }
-
-    return result;
 }
 
 const getOutgoingPart = (heigth, width, x, y) => {
@@ -72,7 +57,7 @@ function ant(grid, column, row, n, dir = 0) {
     }
 
     //invert its value
-    grid[row][column] = Math.abs([row][column] - 1);
+    grid[row][column] = Math.abs(grid[row][column] - 1);
 
     // perform step
     switch (dir) {
@@ -82,13 +67,15 @@ function ant(grid, column, row, n, dir = 0) {
         case 3: column -= 1; break;
     }
 
-    // Check ant coordinates boundaries
-    const columns = grid.length;
-    const rows = grid[0].length;
+    const outgoingPart = getOutgoingPart(grid.length, grid[0].length, row, column);
 
+    if (outgoingPart !== null) {
+        extendArray(grid, outgoingPart);
+        const newAntPosition = getNewAntPosition(outgoingPart, row, column);
 
+        row = newAntPosition.x;
+        column = newAntPosition.y;
+    }
 
     return ant(grid, column, row, n - 1, dir);
 }
-
-console.log(extendArray([[1,1],[1,1]]))
