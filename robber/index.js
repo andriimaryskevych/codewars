e86bb13001ebd1fc2493a5b86306fe27cf2ac6fc
Andrii Maryskevych <andrii.maryskevych@globallogic.com>
2019-07-26 14:04:54 +0300

    Visualize this issue

--- robber/index.js
+++ robber/index.js
@@ -1,17 +1,24 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 
-const robberPosition = [80, 10];
+const robberPosition = [10, 10];
 const robberSpeed = 1;
 
-const doorPosition = [100, 200];
+const doorPosition = [100, 180];
+
+const guardPosition = [200, 0];
+const guardSpeed = 1.4;
 
 const directionVector = getVector(robberPosition, doorPosition);
 const unitDirectionVector = getUnitVector(directionVector);
 
 const totalSteps = getVectorLength(directionVector) / robberSpeed;
 
-for (let i = 0; i < Math.floor(totalSteps); i++) {
+// for simplicity, draw only first 10 seconds if the run
+
+let guardIterationStepPosition = guardPosition;
+
+for (let i = 0; i < 2000; i++) {
     const iterationStep = multiplyVector(unitDirectionVector, i * robberSpeed);
     const stepPosition = addVectors(robberPosition, iterationStep);
 
@@ -21,4 +28,34 @@ for (let i = 0; i < Math.floor(totalSteps); i++) {
         1,
         1
     );
+
+    // calculate guard position
+
+    const guardIterationStepVector = getVector(guardIterationStepPosition, stepPosition);
+    const unitGuardDirectionVector = getUnitVector(guardIterationStepVector);
+    const guardIterationStep = multiplyVector(unitGuardDirectionVector, guardSpeed);
+    const nextGuardPosition = addVectors(guardIterationStepPosition, guardIterationStep);
+
+    ctx.fillRect(
+        nextGuardPosition[0],
+        nextGuardPosition[1],
+        1,
+        1
+    );
+
+    guardIterationStepPosition = nextGuardPosition;
+
+    const length = getVectorLength(getVector(stepPosition, guardIterationStepPosition));
+
+    if (length < 1) {
+        setTimeout(() => { alert('Catched'); });
+
+        break;
+    }
+
+    if (stepPosition[1] >= 200) {
+        setTimeout(() => { alert('Free'); });
+
+        break;
+    }
 }
