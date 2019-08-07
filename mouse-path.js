f0144fc0c61d99aa1c6759d847c8f7c2fc245b77
Andrii Maryskevych <andrii.maryskevych@globallogic.com>
2019-08-07 14:04:21 +0300

    Add Pointed path

--- mouse-path.js
+++ mouse-path.js
@@ -1,9 +1,66 @@
-function mousePath(str){
-    const chunks = [];
-
-    for (let i = 0, charsLength = str.length; i < charsLength; i += 2) {
-        chunks.push(str.substring(i, i + 2));
+const getNewDirection = (direction, newDirection) => {
+    switch (newDirection) {
+        case 'R': direction += 1; break;
+        case 'L': direction -= 1; break;
     }
+
+    if (direction > 3) {
+        direction = 0;
+    }
+
+    if (direction < 0) {
+        direction = 3;
+    }
+
+    return direction;
+};
+
+const performStep = (previousLocation, steps, direction) => {
+    const newlocation = previousLocation.concat();
+
+    switch (direction) {
+        case 0: newlocation[1] += steps; break;
+        case 1: newlocation[0] += steps; break;
+        case 2: newlocation[1] -= steps; break;
+        case 3: newlocation[0] -= steps; break;
+    }
+
+    return newlocation;
+};
+
+/**
+ *  Directinos:
+ *
+ *      0
+ *      |
+ *  3---+---1
+ *      |
+ *      2
+ */
+function mousePath(str){
+    let position = [0, 0];
+
+    // 0 - 3
+    let direction = 0;
+
+    // all positions
+    const vertexes = [
+        position,
+    ];
+
+    const chunks = ('R' + str).match(/[A-Z]\d+/g);
+
+    chunks.forEach(chunk => {
+        const newDirection = chunk.match(/[A-Z]/)[0];
+        const steps = Number(chunk.match(/\d+/)[0]);
+
+        direction = getNewDirection(direction, newDirection);
+        position = performStep(position, steps, direction);
+
+        vertexes.push(position);
+    });
+
+    console.log(vertexes);
 }
 
-console.log(mousePath('4R2L1R5R9R4R4L3'));
+console.log(mousePath('14R11R10R4L1L4R3R7R10R3R3L1L4L5L11R3'));
